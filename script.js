2e3a01cfb260cdbf4c10281102ba21727daf19b5
“Shayda” <smoezzi@mit.edu>
2024-10-23 16:02:03 -0400

    changing dot format

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const container = document.querySelector('.dot-container');
-    
-    for (let i = 0; i < 50; i++) {
+    const dotGrid = document.querySelector('.dot-grid');
+    const numRows = Math.floor(window.innerHeight / 20);  
+    const numCols = Math.floor(window.innerWidth / 20);   
+
+    for (let i = 0; i < numRows * numCols; i++) {
         const dot = document.createElement('div');
         dot.classList.add('dot');
-        container.appendChild(dot);
+        dotGrid.appendChild(dot);
     }
 });
